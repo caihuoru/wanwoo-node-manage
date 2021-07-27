7a7189fe34e6508340bbcc6bc4d6b1348ca85b4d
racaly <zhaoyan@iottepa.com>
2021-07-27 17:27:49 +0800

    fixed:取消测试webhook:node

--- schedule/index.js
+++ schedule/index.js
@@ -2,19 +2,19 @@ const schedule = require('node-schedule');
 const { ToadScheduler, SimpleIntervalJob, AsyncTask } = require('toad-scheduler')
 const {getServiceStatus, setServiceStatus} = require('./service/serverload')
 const scheduleNode = (app)=>{
-    schedule.scheduleJob('*/5 * * * * ?',async ()=>{
+    //schedule.scheduleJob('*/5 * * * * ?',async ()=>{
         //触发多个mq
-        app.context.simplestMq.sendQueueMsg('webhook:node',{
-                type:"user:login",
-                id:1,
-                data:{
-                    userName: "用户名",
-                    time: "时间戳"
-                }
-        })
+        // app.context.simplestMq.sendQueueMsg('webhook:node',{
+        //         type:"user:login",
+        //         id:1,
+        //         data:{
+        //             userName: "用户名",
+        //             time: "时间戳"
+        //         }
+        // })
         // app.context.workqueuetMq.sendQueueMsg('JvanTest1',['11111','22222','33333','44444','55555'])
         // app.context.pubsubMq.sendQueueMsg('JvanTest2','queue',['211111','222222','233333','244444','255555'])
-    });
+    //});
     // schedule.scheduleJob('*/10 * * * * ?',async ()=>{
     //     getServiceStatus()
     // })
