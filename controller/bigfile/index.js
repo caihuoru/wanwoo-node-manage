f275a8e1c91a4e4cca81f6d923aacf4ba9c95cd7
racaly <zhaoyan@iottepa.com>
2021-07-27 15:42:01 +0800

    添加分布式锁

--- controller/bigfile/index.js
+++ controller/bigfile/index.js
@@ -1,21 +1,55 @@
 const fse = require('fs-extra');
 const path = require('path'); // 图片路径
-module.exports ={
+module.exports = {
     //上传分片
-    updateFile: async(ctx, next) =>{
-        console.log(ctx.request.files)
+    updateFile: async (ctx, next) => {
+
+        const { ip, body } = ctx.request
+        const visitIp = ip.replace('::ffff:', '');
+        let LOCK_DATA = await ctx.redisStore.get('SYSTEM_UPDATE_LOCK_DATA')
         const fileNameArr = ctx.request.files.file.name.split('.')
         const chunkDir = `./public/share/temp/${fileNameArr[0]}`
+        const NEW_LOCK_DATA = {
+            visitIp: visitIp,
+            type: body.type,
+            index: body.index,
+            userId: body.userid
+        }
+       
+        if (LOCK_DATA) {
+           
+            if (LOCK_DATA.type == body.type) {
+                console.log('*******************************',LOCK_DATA)
+                if (LOCK_DATA.userId != body.userid) {
+                    // 有用户正在升级,锁定
+                    ctx.fail('上传失败', 50001, '已存在升级上传任务，请稍后重试！')
+                    return
+                } else {
+                    if (LOCK_DATA.visitIp != visitIp && fse.existsSync(chunkDir)) {
+                        // ip发生变化，清空重新上传
+                        await fse.remove(chunkDir)
+                    }
+                    if (body.index === '0' && fse.existsSync(chunkDir)) {
+                        // 切片发生变化，清空
+                        await fse.remove(chunkDir)
+                    }
+                }
+            }
+        } else if (body.index !== '0') {
+            ctx.fail('上传失败', 50001, '任务已过期，请重新上传')
+        }
+        ctx.redisStore.set('SYSTEM_UPDATE_LOCK_DATA', NEW_LOCK_DATA, NEW_LOCK_DATA.type, 3600 * 24)
         if (!fse.existsSync(chunkDir)) {// 没有目录就创建目录
             await fse.mkdirs(chunkDir)
         }
         const dPath = path.join(chunkDir, fileNameArr[1])
         await fse.move(ctx.request.files.file.path, dPath, { overwrite: true })
         ctx.success({})
+
     },
     //合并切片
-    mergeFile: async(ctx, next) =>{
-        const { name }= ctx.request.body
+    mergeFile: async (ctx, next) => {
+        const { name } = ctx.request.body
         const fname = name.split('.')[0]
         const chunkDir = path.join(`./public/share/temp/`, fname)
         const chunks = await fse.readdir(chunkDir)
@@ -27,9 +61,9 @@ module.exports ={
             )
         })
         fse.removeSync(chunkDir)
-        if(fse.existsSync('/mnt/download')){
+        if (fse.existsSync('/mnt/download')) {
             await fse.move(`./public/share/${name}`, `/mnt/download/${name}`, { overwrite: true })
         }
-        ctx.success({url:`/mnt/download/${name}`})
+        ctx.success({ url: `/mnt/download/${name}` })
     }
 }
