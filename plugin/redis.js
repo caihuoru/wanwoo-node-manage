53baaada23e6a66e0f5c243703e9509ecbe7c00a
racaly <zhaoyan@iottepa.com>
2021-08-02 19:49:50 +0800

    fix:redis支持集群

--- plugin/redis.js
+++ plugin/redis.js
@@ -7,67 +7,92 @@ const logUtil = require('./log4j');
 const config = require('../config')
 
 class RedisStore {
-    constructor() {
-        this.redis = new Redis({
-            port: config.RD_PORT, // Redis port
-            host: config.RD_HOST, // Redis host
-            family: config.RD_FAMILY, // 4 (IPv4) or 6 (IPv6)
-            password: config.RD_PASSWORD,
-            db: config.RD_DB,
-            retryStrategy(times){
-              const delay = Math.min(times * 50, 2000);
-              return delay;
-            },
-            reconnectOnError(err) {
-              const targetError = "READONLY";
-              if (err.message.includes(targetError)) {
-                return true;
-              }
-            }
-        });
-        this.redis.connect(() => {
-          logUtil.pluginLogger.info('Redis','connect','redis启动成功！')
+  constructor() {
+    try {
+      const REDIS_MEMBERS = global.REDIS_MEMBERS
+      const basicConf = {
+        family: global.RD_FAMILY, // 4 (IPv4) or 6 (IPv6)
+        password: global.RD_PASSWORD,
+        db: global.RD_DB,
+        retryStrategy(times) {
+          const delay = Math.min(times * 50, 2000);
+          return delay;
+        },
+        reconnectOnError(err) {
+          const targetError = "READONLY";
+          if (err.message.includes(targetError)) {
+            return true;
+          }
+        }
+      }
+      if (Array.isArray(REDIS_MEMBERS)) {
+       const MEMBERS =  REDIS_MEMBERS.map(irm => {
+          return {
+            port: irm.port, // Redis port
+            host: irm.host, // Redis host
+           
+          }
         })
-      }
-      async set(sid,obj,type="",time){
-        try {
-          if(type!=''){
-            await this.redis.set(sid, JSON.stringify(obj),type,time)
-          }else{
-            await this.redis.set(sid, JSON.stringify(obj))
-          }
-          logUtil.pluginLogger.info('Redis','set-'+sid,JSON.stringify(obj))
-        } catch (e) {
-          logUtil.pluginLogger.error('Redis','set-'+sid,JSON.stringify(e))
-        }
-        return sid;
-      }
-      async get(sid){
-        let data = await this.redis.get(sid);
-        logUtil.pluginLogger.info('Redis','get-'+sid,data)
-        if (data && typeof JSON.parse(data) == "object") {
-          return JSON.parse(data);
+        console.log('**************', MEMBERS)
+        if(MEMBERS.length===1){
+          this.redis = new Redis({...MEMBERS[0],...basicConf});
         }else{
-          if(data){
-            return data
-          }else{
-            return false
-          }
-          
+          this.redis =  new Redis.Cluster(MEMBERS,basicConf);
         }
-      }
-      async incr(sid){
-        try {
-          await this.redis.incr(sid)
-          logUtil.pluginLogger.info('Redis','incr-'+sid,JSON.stringify(obj))
-        } catch (e) {
-          logUtil.pluginLogger.error('Redis','incr-'+sid,JSON.stringify(e))
-        }
-        return sid;
         
+        
+      } else {
+        logUtil.pluginLogger.info('Redis', 'connect', 'redis参数异常！')
       }
+    } catch (error) {
+     
+      console.log('**************', global.REDIS_MEMBERS)
+      console.log('=============',error)
+      logUtil.pluginLogger.info('Redis', 'connect', error.message)
+    }
+    this.redis.connect(() => {
+      logUtil.pluginLogger.info('Redis', 'connect', 'redis启动成功！')
+    })
+  }
+  async set(sid, obj, type = "", time) {
+    try {
+      if (type != '') {
+        await this.redis.set(sid, JSON.stringify(obj), type, time)
+      } else {
+        await this.redis.set(sid, JSON.stringify(obj))
+      }
+      logUtil.pluginLogger.info('Redis', 'set-' + sid, JSON.stringify(obj))
+    } catch (e) {
+      logUtil.pluginLogger.error('Redis', 'set-' + sid, JSON.stringify(e))
+    }
+    return sid;
+  }
+  async get(sid) {
+    let data = await this.redis.get(sid);
+    logUtil.pluginLogger.info('Redis', 'get-' + sid, data)
+    if (data && typeof JSON.parse(data) == "object") {
+      return JSON.parse(data);
+    } else {
+      if (data) {
+        return data
+      } else {
+        return false
+      }
+
+    }
+  }
+  async incr(sid) {
+    try {
+      await this.redis.incr(sid)
+      logUtil.pluginLogger.info('Redis', 'incr-' + sid, JSON.stringify(obj))
+    } catch (e) {
+      logUtil.pluginLogger.error('Redis', 'incr-' + sid, JSON.stringify(e))
+    }
+    return sid;
+
+  }
 }
 let redisStore = new RedisStore()
 module.exports = {
-    redisStore:redisStore
+  redisStore: redisStore
 }
