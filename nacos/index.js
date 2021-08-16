63cae1a42824c472fd8be7acfebd05cea2add1d6
racaly <zhaoyan@iottepa.com>
2021-08-16 09:50:21 +0800

    远程配置获取失败使用本地配置

--- nacos/index.js
+++ nacos/index.js
@@ -1,16 +1,22 @@
 const NacosNamingClient = require('nacos').NacosNamingClient;
 const NacosConfigClient = require('nacos').NacosConfigClient;
+const logUtil = require('../plugin/log4j')
 module.exports = {
   initNacosConfig: async () => {
     const configClient = new NacosConfigClient({
       serverAddr: global.NACOS_IP
     });
-    const content = await configClient.getConfig(global.NACOS_CONFIG, global.NACOS_GROUP_NAME);
-    const processConfig = JSON.parse(content)
-    //再次使用远程覆盖本地赋值
-    for (const k in processConfig) {
-      global[k] = processConfig[k]
+    try {
+      const content = await configClient.getConfig(global.NACOS_CONFIG, global.NACOS_GROUP_NAME);
+      const processConfig = JSON.parse(content)
+      //再次使用远程覆盖本地赋值
+      for (const k in processConfig) {
+        global[k] = processConfig[k]
+      }
+    } catch (_) {
+      logUtil.pluginLogger.info('nacos', 'error', '参数获取异常，已启用本地配置')
     }
+
   },
   initNacosInstance: async () => {
     const client = new NacosNamingClient({
