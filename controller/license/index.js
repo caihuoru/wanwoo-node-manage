6d20b20b81a1b315345c679efbade41253e6de15
Fmm <18702530142@139.com>
2021-11-30 15:59:10 +0800

    fix:修复 系统授权上传前几次报错

--- controller/license/index.js
+++ controller/license/index.js
@@ -6,8 +6,13 @@ module.exports ={
         let chunkDir = path.join('./public/share/license/uecm_license.lid');
         await fse.move(ctx.request.files.file.path, chunkDir, { overwrite: true })
         if(fse.existsSync('/mnt/download')){
-            fse.move(`./public/share/license/uecm_license.lid`, `/mnt/download/uecm_license.lid`, { overwrite: true })
+            await fse.move(`./public/share/license/uecm_license.lid`, `/mnt/download/uecm_license.lid`, { overwrite: true })
+            await new Promise(function(reslove){
+                setTimeout(()=>{reslove()},10000)
+            })
+            ctx.success({url:`/mnt/download/uecm_license.lid`})
+        }else{
+            ctx.fail('系统错误',500,error.message)
         }
-        ctx.success({url:`/mnt/download/uecm_license.lid`})
     },
 }
